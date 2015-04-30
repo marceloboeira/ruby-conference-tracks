2b9dadcf860d4396ae4b173e4b1da8a390a2c9b6
Marcelo Boeira <me@marceloboeira.com>
2015-04-30 20:14:11 -0300

    Parser and length handler

--- lib/talk.rb
+++ lib/talk.rb
@@ -1,3 +1,19 @@
 class Talk 
-  attr_accessor :name, :length
+  attr_accessor :title, :length
+
+  def initialize(options = {})
+    name = options[:name] || "No name"
+    length = options[:length] || "10min"
+  end
+
+  def length=(value)
+    @length = value.delete('^0-9').to_i
+  end
+
+  # Default pattern: "Name of my conference 40min"
+  def self.parse input
+    title  = input.split(" ")
+    length = title.pop
+    Talk.new(title: title.join(" "), length: length)
+  end
 end
