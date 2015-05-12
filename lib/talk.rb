03f4e8a50a9e091234ad3485ddf61a5b9d0fd5c8
Marcelo Boeira <me@marceloboeira.com>
2015-05-12 00:19:51 -0300

    Improving talk class

--- lib/talk.rb
+++ lib/talk.rb
@@ -1,19 +1,32 @@
 class Talk 
-  attr_accessor :title, :length
+  attr_reader :title, :length
+  attr_accessor :starts_at
 
   def initialize(options = {})
-    title = options[:title] || "No name"
-    length = options[:length] || "10min"
+    options.each { |option, value| instance_variable_set("@#{option}", value) }
+    normalize
   end
 
-  def length=(value)
-    @length = value.delete('^0-9').to_i
+  def to_s
+    "#{@starts_at.strftime('%l:%M %p')} - #{@title}"
   end
 
-  # Default pattern: "Name of my conference 40min"
-  def self.parse input
-    title  = input.split(" ")
-    length = title.pop
-    Talk.new(title: title.join(" "), length: length)
+  private
+
+  def normalize
+    @title ||= "No name"
+    @length ||= "10min"
+    @starts_at ||= "12:00 AM"
+    @starts_at = Time.parse(@starts_at) if @starts_at.is_a? String
+    @length = (@length == "lightning" ? 5 : @length.delete("^0-9").to_i) * 60
+  end
+ 
+  # Valid patterns:
+  # "Name of my talk 40min"
+  # "Name of my talk (40min)"
+  # "Name of my talk - 40min"
+  def self.parse(input)
+    line = input.split(" ")
+    Talk.new(length: line.pop, title: line.join(" "))
   end
 end
